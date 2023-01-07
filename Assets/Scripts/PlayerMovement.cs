7fc19a7ccc62f86dfa14562b09c66267bf0c437f
Jack Wetherell <jack.wetherell@gmail.com>
2023-01-07 21:48:36 +0000

    Initial code.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 500.0F;
+    public float sidewaysForce = 500.0F;
+
+    void FixedUpdate()
+    {
+        rb.AddForce(0.0F, 0.0F, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0.0F, 0.0F, ForceMode.VelocityChange);
+        }
+
+        else if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0.0F, 0.0F, ForceMode.VelocityChange);
+        }
+    }
+}
